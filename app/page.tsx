f02d6127c2af66f22264a286ff9d036509bb946e
Demis Rincon <darmfma@gmail.com>
2025-01-27 10:10:22 -0600

    fragment

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@
 import PageBuilder from "@/components/PageBuilder";
 import fetchTool, { FetchType } from "@/library/contentful/fetchTool";
 import { getPageBySlug } from "@/library/contentful/querys";
+import { Fragment } from "react";
 
 const Home = () => {
   const data = fetchTool(getPageBySlug("/"), FetchType.dynamicData);
   return (
-    <>
+    <Fragment>
       <PageBuilder data={data} />
-    </>
+    </Fragment>
   );
 };
 
