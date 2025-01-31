63c0e92e661016c8cccf980e4a437d8bf41b80e1
Demis Rincon <darmfma@gmail.com>
2025-01-30 23:45:09 -0600

    fix  mobile

--- components/HeroWithTitleButton.tsx
+++ components/HeroWithTitleButton.tsx
@@ -57,7 +57,7 @@ const HeadingContainer = styled(motion.div)`
   align-items: center;
   justify-content: center;
   box-sizing: border-box;
-  width: 100%;
+  width: 90%;
   height: 80%;
   @media (min-width: ${({ theme }) => theme.breakpoints.lg}) {
     height: 50%;
@@ -68,11 +68,12 @@ const Heading = styled.h1<{ $color?: string }>`
   color: ${({ theme, $color }) =>
     $color ? theme.colors[$color] : theme.colors.black};
   box-sizing: border-box;
-  display: block;
-  font-size: 4rem;
+  font-size: 3.5rem;
   text-align: center;
   box-sizing: border-box;
   margin: 0;
+  max-width: 100%;
+  text-overflow: ellipsis;
   @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
     font-size: 4rem;
     padding-top: 30px;
@@ -116,21 +117,23 @@ const HeroWithTitleButton: React.FC<HeroWithTitleButtonProps> = ({
   return (
     <PageContainer $isFirstElement={orderInPage === 1} $bgColor={bgColor}>
       <Container>
-        <ButtonContainer>
-          <WrapperFadeIn className="full-width">
-            <Button
-              onClick={() => (window.location.href = button.url)}
-              $bgColor={button.bgColor}
-              $color={button.color}
-              whileHover={{ scale: 1.2, cursor: "pointer" }}
-              whileTap={{ scale: 0.8 }}
-              title={`Click to go to ${subHeading}`}
-              style={{ y: yButton }}
-            >
-              {button.text}
-            </Button>
-          </WrapperFadeIn>
-        </ButtonContainer>
+        {button.text && (
+          <ButtonContainer>
+            <WrapperFadeIn className="full-width">
+              <Button
+                onClick={() => (window.location.href = button.url)}
+                $bgColor={button.bgColor}
+                $color={button.color}
+                whileHover={{ scale: 1.2, cursor: "pointer" }}
+                whileTap={{ scale: 0.8 }}
+                title={`Click to go to ${subHeading}`}
+                style={{ y: yButton }}
+              >
+                {button.text}
+              </Button>
+            </WrapperFadeIn>
+          </ButtonContainer>
+        )}
         <HeadingContainer style={{ scale: headingScale }}>
           <WrapperFadeIn>
             <Heading $color={fontColor}>
