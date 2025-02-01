7b31a8a439ccf5c3ddba72104c22451cafa129c7
Demis Rincon <darmfma@gmail.com>
2025-01-31 22:41:04 -0600

    fixed

--- app/@header/desktopNavigator.tsx
+++ app/@header/desktopNavigator.tsx
@@ -1,7 +1,67 @@
+import headerData from "@/library/data/header";
 import React from "react";
+import styled from "styled-components";
+import Link from "next/link";
+import { motion } from "motion/react";
+
+const Container = styled.nav`
+  display: flex;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  height: 100%;
+  text-transform: uppercase;
+`;
+const NavLink = styled(motion.div)<{ $isActive?: boolean }>`
+  text-decoration: none;
+  font-weight: bold;
+
+  font-size: 1rem;
+  color: ${(props) => props.theme.colors.black};
+  ${(props) =>
+    props.$isActive &&
+    `
+      color: ${props.theme.colors.teal};
+      font-size: 1.2rem;
+    `}
+  &:after {
+    content: "";
+    display: block;
+    width: 0;
+    height: 2px;
+    background: ${(props) => props.theme.colors.teal};
+    transition: width 0.3s;
+  }
+  &:hover:after {
+    width: 100%;
+  }
+`;
 
 const DesktopNavigator = () => {
-  return <div>desktop</div>;
+  return (
+    <Container>
+      {headerData.links.map((link) => {
+        return (
+          <NavLink
+            key={link.href}
+            whileHover={{
+              scale: 1.1,
+              y: -5,
+              transition: { duration: 0.2 },
+            }}
+          >
+            <Link
+              href={link.href}
+              style={{ textDecoration: "none", color: "inherit" }}
+              prefetch={true}
+            >
+              {link.label}
+            </Link>
+          </NavLink>
+        );
+      })}
+    </Container>
+  );
 };
 
 export default DesktopNavigator;
--- library/providers/GlobalStyles.tsx
+++ library/providers/GlobalStyles.tsx
@@ -7,11 +7,22 @@ export const GlobalStyles = createGlobalStyle`
         letter-spacing: 2px;
         margin: 0;
         padding: 0;
+        max-width: 100vw;
     }
     h1, h2, h3, h4, h5, h6 {
         font-family: ${({ theme }) => theme.fonts.heading};
         padding: 0;
         margin: 0;
+        text-transform: uppercase;
+        strong {
+            color: transparent;
+            text-shadow: 0 0 0 1px ${(props) => props.theme.colors.black};
+            @supports (-webkit-text-stroke: 1px ${(props) =>
+              props.theme.colors.black}) {
+                -webkit-text-fill-color: transparent;
+                -webkit-text-stroke: 1px ${(props) => props.theme.colors.black};
+            }
+        }
     }
 `;
 export default GlobalStyles;
