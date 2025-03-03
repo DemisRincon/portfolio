d4ceef1b70356dd3a5f24c9776cba56f4d8a7298
Demis Rincon <darmfma@gmail.com>
2025-03-03 00:49:18 -0600

    testing seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
+import Head from "next/head";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { Bebas_Neue, Poppins } from "next/font/google";
+
 const bebasNeue = Bebas_Neue({
   weight: "400",
   style: "normal",
@@ -20,6 +22,38 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Demis Rincon",
   description: "Portfolio of Demis Rincon",
+  keywords: [
+    "Demis Rincon",
+    "Portfolio",
+    "Web Developer",
+    "software developer",
+    "full stack software developer",
+    "software developer resume",
+    "software developer portfolio",
+    "software developer website",
+    "software developer personal website",
+    "software developer blog",
+    "software developer personal blog",
+    "software developer projects",
+    "software developer skills",
+    "software developer about me",
+    "software developer contact",
+    "software developer social media",
+    "software developer github",
+    "software developer linkedin",
+    "Frontend Developer",
+    "React",
+    "Next.js",
+    "JavaScript",
+    "TypeScript",
+    "CSS",
+    "HTML",
+    "Web Development",
+    "Programming",
+    "Software Engineer",
+    "UI/UX",
+    "Tech",
+  ],
 };
 
 export default function RootLayout({
@@ -29,7 +63,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`font-sans ${bebasNeue} ${poppins}`}>
+      <body className={`font-sans ${bebasNeue.variable} ${poppins.variable}`}>
         <Navbar />
         {children}
         <Footer />
