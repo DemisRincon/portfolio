217c5df334e6b392ab491f6ad3410e353bf9dfc2
Demis Rincon <darmfma@gmail.com>
2025-03-09 00:53:01 -0600

    a11y

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,73 +1,72 @@
-import type { Metadata } from 'next';
-import Head from 'next/head';
-import './globals.css';
-import { Navbar } from '@/components/navbar';
-import { Footer } from '@/components/footer';
-import { Bebas_Neue, Poppins } from 'next/font/google';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Navbar } from "@/components/navbar";
+import { Footer } from "@/components/footer";
+import { Bebas_Neue, Poppins } from "next/font/google";
 
 const bebasNeue = Bebas_Neue({
-	weight: '400',
-	style: 'normal',
-	subsets: ['latin'],
-	variable: '--font-bebas-neue',
+	weight: "400",
+	style: "normal",
+	subsets: ["latin"],
+	variable: "--font-bebas-neue",
 });
 
 const poppins = Poppins({
-	weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-	style: 'normal',
-	subsets: ['latin'],
-	variable: '--font-poppins',
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	style: "normal",
+	subsets: ["latin"],
+	variable: "--font-poppins",
 });
 
 export const metadata: Metadata = {
 	metadataBase: new URL(
-		process.env.SITE_URL || 'https://demisrincon.netlify.app',
+		process.env.SITE_URL || "https://demisrincon.netlify.app",
 	),
-	title: 'Demis Rincon',
-	description: 'Portfolio of Demis Rincon',
+	title: "Demis Rincon",
+	description: "Portfolio of Demis Rincon",
 	keywords: [
-		'Demis Rincon',
-		'Portfolio',
-		'Web Developer',
-		'software developer',
-		'full stack software developer',
-		'software developer resume',
-		'software developer portfolio',
-		'software developer website',
-		'software developer personal website',
-		'software developer blog',
-		'software developer personal blog',
-		'software developer projects',
-		'software developer skills',
-		'software developer about me',
-		'software developer contact',
-		'software developer social media',
-		'software developer github',
-		'software developer linkedin',
-		'Frontend Developer',
-		'React',
-		'Next.js',
-		'JavaScript',
-		'TypeScript',
-		'CSS',
-		'HTML',
-		'Web Development',
-		'Programming',
-		'Software Engineer',
-		'UI/UX',
-		'Tech',
+		"Demis Rincon",
+		"Portfolio",
+		"Web Developer",
+		"software developer",
+		"full stack software developer",
+		"software developer resume",
+		"software developer portfolio",
+		"software developer website",
+		"software developer personal website",
+		"software developer blog",
+		"software developer personal blog",
+		"software developer projects",
+		"software developer skills",
+		"software developer about me",
+		"software developer contact",
+		"software developer social media",
+		"software developer github",
+		"software developer linkedin",
+		"Frontend Developer",
+		"React",
+		"Next.js",
+		"JavaScript",
+		"TypeScript",
+		"CSS",
+		"HTML",
+		"Web Development",
+		"Programming",
+		"Software Engineer",
+		"UI/UX",
+		"Tech",
 	],
 	openGraph: {
-		title: 'Demis Rincon',
-		description: 'Portfolio of Demis Rincon',
-		url: process.env.SITE_URL || 'https://demisrincon.netlify.app',
-		type: 'website',
-		locale: 'en_US',
+		title: "Demis Rincon",
+		description: "Portfolio of Demis Rincon",
+		url: process.env.SITE_URL || "https://demisrincon.netlify.app",
+		type: "website",
+		locale: "en_US",
 	},
 	twitter: {
-		card: 'summary_large_image',
-		title: 'Demis Rincon',
-		description: 'Portfolio of Demis Rincon',
+		card: "summary_large_image",
+		title: "Demis Rincon",
+		description: "Portfolio of Demis Rincon",
 	},
 };
 
@@ -78,17 +77,16 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<Head>
-				<meta
-					name="google-site-verification"
-					content="LTYcCzcKBQEGXW5HyZyQBT3WTz5pNr7_-7Q0fS9kj9M"
-				/>
-			</Head>
-			<body
-				className={`font-sans ${bebasNeue.variable} ${poppins.variable}`}
-			>
+			<body>
 				<Navbar />
-				<main role="main">{children}</main>
+
+				<main
+					className={`${bebasNeue.variable} ${poppins.variable} font-sans`}
+					role="main"
+					aria-label="Main content"
+				>
+					{children}
+				</main>
 				<Footer />
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -6,59 +6,103 @@ import IconWall from "@/components/iconWall";
 import { getPage } from "./actions";
 
 const Home = async () => {
-  const [
-    heroData,
-    aboutData,
-    iconWallData1,
-    iconWallData2,
-    iconWallData3,
-    iconWallData4,
-    projectsData,
-    contactData,
-  ] = await getPage();
+	const [
+		heroData,
+		aboutData,
+		iconWallData1,
+		iconWallData2,
+		iconWallData3,
+		iconWallData4,
+		projectsData,
+		contactData,
+	] = await getPage();
 
-  return (
-    <div className="min-h-screen bg-white">
-      <main>
-        <section
-          id="home"
-          className="py-24 bg-sand md:py-32 lg:py-40 min-h-screen"
-        >
-          <Hero {...heroData} />
-        </section>
-        <section
-          id="about"
-          className="py-24 md:py-32 lg:py-40 bg-white min-h-screen"
-        >
-          <About {...aboutData} />
-        </section>
-        <section id="iconwall1" className="bg-coal-700">
-          <IconWall {...iconWallData1} />
-        </section>
-        <section id="iconwall2" className="bg-coal-700">
-          <IconWall {...iconWallData2} />
-        </section>
-        <section id="iconwall3" className="bg-coal-700">
-          <IconWall {...iconWallData3} />
-        </section>
-        <section id="iconwall4" className="bg-coal-700">
-          <IconWall {...iconWallData4} />
-        </section>
-        <section
-          id="projects"
-          className="py-24 md:py-32 lg:py-40  min-h-screen bg-secondary-200"
-        >
-          <Projects {...projectsData} />
-        </section>
-        <section
-          id="contact"
-          className="py-24 md:py-32 lg:py-40  bg-slate-100 min-h-screen"
-        >
-          <Contact {...contactData} />
-        </section>
-      </main>
-    </div>
-  );
+	return (
+		<div className="min-h-screen bg-white">
+			<main>
+				<section
+					id="home"
+					className="py-24 bg-sand md:py-32 lg:py-40 min-h-screen"
+					aria-labelledby="home-heading"
+				>
+					<h1 id="home-heading" className="sr-only">
+						Home
+					</h1>
+					<Hero {...heroData} />
+				</section>
+				<section
+					id="about"
+					className="py-24 md:py-32 lg:py-40 bg-white min-h-screen"
+					aria-labelledby="about-heading"
+				>
+					<h2 id="about-heading" className="sr-only">
+						About
+					</h2>
+					<About {...aboutData} />
+				</section>
+				<section
+					id="iconwall1"
+					className="bg-coal-700"
+					aria-labelledby="iconwall1-heading"
+				>
+					<h2 id="iconwall1-heading" className="sr-only">
+						Icon Wall 1
+					</h2>
+					<IconWall {...iconWallData1} />
+				</section>
+				<section
+					id="iconwall2"
+					className="bg-coal-700"
+					aria-labelledby="iconwall2-heading"
+				>
+					<h2 id="iconwall2-heading" className="sr-only">
+						Icon Wall 2
+					</h2>
+					<IconWall {...iconWallData2} />
+				</section>
+				<section
+					id="iconwall3"
+					className="bg-coal-700"
+					aria-labelledby="iconwall3-heading"
+				>
+					<h2 id="iconwall3-heading" className="sr-only">
+						Icon Wall 3
+					</h2>
+					<IconWall {...iconWallData3} />
+				</section>
+				<section
+					id="iconwall4"
+					className="bg-coal-700"
+					aria-labelledby="iconwall4-heading"
+				>
+					<h2 id="iconwall4-heading" className="sr-only">
+						Icon Wall 4
+					</h2>
+					<IconWall {...iconWallData4} />
+				</section>
+				<section
+					id="projects"
+					className="py-24 md:py-32 lg:py-40 min-h-screen bg-secondary-200"
+					aria-labelledby="projects-heading"
+				>
+					<h2 id="projects-heading" className="sr-only">
+						Projects
+					</h2>
+					<Projects {...projectsData} />
+				</section>
+				<section
+					id="contact"
+					className="py-24 md:py-32 lg:py-40 bg-slate-100 min-h-screen"
+					aria-labelledby="contact-heading"
+				>
+					<h2 id="contact-heading" className="sr-only">
+						Contact
+					</h2>
+					<Contact {...contactData} />
+				</section>
+			</main>
+		</div>
+	);
 };
 
 export default Home;
