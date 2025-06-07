7228030735d22599a7b2cfc84cba8689c9b608c5
Demis Rincon <darmfma@gmail.com>
2025-06-07 14:12:31 -0600

    remove conditional webpack watch options for development environment

--- next.config.ts
+++ next.config.ts
@@ -15,15 +15,7 @@ interface ImagesConfig {
 }
 
 const nextConfig: NextConfig = {
-	webpack: (config, { dev, isServer }) => {
-		if (dev && !isServer) {
-			config.watchOptions = {
-				poll: 1000,
-				aggregateTimeout: 300,
-			};
-		}
-		return config;
-	},
+
 	images: {
 		remotePatterns: [
 			{
